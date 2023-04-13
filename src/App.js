a7b627de33d8f43ca32fd85405bafbef7139df6a
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-04-13 23:25:40 +0530

    Basic UI implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, {Fragment} from 'react'
+import React from 'react'
+import FileUpload from './components/FileUpload'
 
 const App = () => {
   return(
-    <Fragment>
-      
-    </Fragment>
+    <>
+      <div className="container">
+        <h4 className="text-center mb-5 display-4">File Uploader</h4> 
+      </div>
+      <FileUpload />
+    </>
   )
 }
 
