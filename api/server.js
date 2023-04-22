3aa9fc7a8a0b67ade347317aed158ab2caf0af99
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-04-23 01:41:34 +0530

    Basic functioning done

--- api/server.js
+++ api/server.js
@@ -1,23 +1,33 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
+const path = require('path')
 
 const app = express()
 
+
 app.use(fileUpload())
 
-app.post('/upload', (req, res) => {
-    if(req.files === null){
-        return res.status(400).json({msg: "No file uploaded"})
-    }
-    const uploadedFile = req.files.uplodedFile
-
-    uploadedFile.mv(`${__dirname}/../public/uploads/${uploadedFile.name}`, err => {
-        if(err){
-            console.error(err);
-            res.status(500).send(err)
+app.post('/uploads', (req, res) => {
+    try{
+        if(req.files === null){
+            return res.status(400).json({msg: "No file uploaded"})
         }
-        res.json({fileName: uploadedFile.name, filePath: `/uploads/${uploadedFile.name}`})
-    })
+        const uploadedFile = req.files.uploadedFile
+    
+        uploadedFile.mv(`${path.dirname(__dirname)}/public/uploads/${uploadedFile.name}`, err => {
+            if(err){
+                console.error(err); 
+                return res.status(500).send(err)
+            }
+            res.json({fileName: uploadedFile.name, filePath: `/uploads/${uploadedFile.name}`})
+        })
+        res.status(200).send("File uploaded")
+    }
+    catch(err){
+        res.status(500).json({error: err.response.data.msg})
+    }
 })
 
-app.listen('8081', () => console.log("Server started..."))
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, () => console.log("Server started... at port " + PORT))
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+
 
 const FileUpload = () => {
-  return (
-    <>
-    <form action="post" className="d-flex flex-column justify-content-center align-items-center">
-        <div class="mb-3 col-md-6 col-sm-10 col-10">
-            <div className="input-group">                
-                <input class="form-control" type="file" id="customFileInput"/>
-            </div>
-        </div>
-        <button type="submit" className="btn btn-primary col-md-2 col-sm-4 col-4">Upload</button>
-    </form>
-    </>
-  )
+    const [file, setFile] = useState({});
+    const [uploadedFile, setUploadedFile] = useState({});
+
+    const onFileChange = e => setFile(e.target.files[0])
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        const formdata = new FormData() 
+        formdata.append('uploadedFile', file)
+
+        try{
+            const response = await axios.post('http://localhost:5500/uploads', formdata, {
+                headers:{
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            const {fileName, filePath} = response.data
+            setUploadedFile({fileName, filePath})
+            console.log(uploadedFile)
+        }
+        catch(err){
+            if(err.response.status === 500){
+                console.error("There was a problem with the server")
+            }
+            else{
+                console.error(err.response.data.msg)
+            }
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={onSubmit} className="d-flex flex-column justify-content-center align-items-center">
+                <div className="mb-3 col-md-6 col-sm-10 col-10">
+                    <div className="input-group">                
+                        <input className="form-control" type="file" id="customFileInput" onChange={onFileChange}/>
+                    </div>
+                </div>
+                <input type="submit" value="Upload" className="btn btn-primary col-md-2 col-sm-4 col-4" />
+            </form>
+        </>
+    )
 }
 
 export default FileUpload
